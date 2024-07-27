3099f267115d8b20eaa46ee887fc96acd9c76bfd
Hüsen Baghayi <husen@adeveloper.org>
2024-07-27 21:01:52 +0330

    Reverse String function

---
+++ exercise_1-19.c
@@ -0,0 +1,75 @@
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+char * reverse(char *);
+int string_size(char *);
+
+int test_reverse_character_string_one_character();
+int test_reverse_character_string_two_characters();
+int test_reverse_character_string_three_characters();
+int test_reverse_character_string_with_enormous_characters();
+
+int main(void) {
+    int test_result = 0;
+    if ((test_result = test_reverse_character_string_one_character()) != 0) return test_result;
+    if ((test_result = test_reverse_character_string_two_characters()) != 0) return test_result;
+    if ((test_result = test_reverse_character_string_three_characters()) != 0) return test_result;
+    if ((test_result = test_reverse_character_string_with_enormous_characters()) != 0) return test_result;
+
+    return 0;
+}
+
+char * reverse(char * string) {
+    int total_characters = string_size(string);
+    
+    char * reverse_string = (char *) malloc(sizeof(char) * (total_characters+1));
+
+    int back_counter = total_characters;
+    for (int i = 0; i < total_characters; i++)
+        reverse_string[i] = string[--back_counter];
+    reverse_string[total_characters] = '\0';
+    return reverse_string;
+}
+
+int string_size(char * string) {
+    int total_characters = 0;
+    while(string[total_characters] != '\0') total_characters++;
+    return total_characters;
+}
+
+int test_reverse_character_string_one_character() {
+    char * input = "H";
+    char * expected = "H";
+    char * reversed_string = reverse(input);
+    int result = strcmp(reversed_string, expected);
+    free(reversed_string);
+    return result;
+}
+
+int test_reverse_character_string_two_characters() {
+    char * input = "Hi";
+    char * expected = "iH";
+    char * reversed_string = reverse(input);
+    int result = strcmp(reversed_string, expected);
+    free(reversed_string);
+    return result;
+}
+
+int test_reverse_character_string_three_characters() {
+    char * input = "Boy";
+    char * expected = "yoB";
+    char * reversed_string = reverse(input);
+    int result = strcmp(reversed_string, expected);
+    free(reversed_string);
+    return result;
+}
+
+int test_reverse_character_string_with_enormous_characters() {
+    char * input = "This is a text!";
+    char * expected = "!txet a si sihT";
+    char * reversed_string = reverse(input);
+    int result = strcmp(reversed_string, expected);
+    free(reversed_string);
+    return result;
+}
